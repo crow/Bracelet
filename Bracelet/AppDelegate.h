8abce7bf119f55a8ce27232c707dcb216e8dcbb4
David Crow <jdavidcrow@gmail.com>
2014-12-18 11:47:30 -0800

    Initial Commit

---
+++ Bracelet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bracelet
+//
+//  Created by David Crow on 12/18/14.
+//  Copyright (c) 2014 dcrow.us. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bracelet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bracelet
+//
+//  Created by David Crow on 12/18/14.
+//  Copyright (c) 2014 dcrow.us. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
