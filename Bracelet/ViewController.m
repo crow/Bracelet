1da9bad2df3e70d9fe9357bc704ad37dd31a6da1
David Crow <jdavidcrow@gmail.com>
2014-12-28 17:58:34 -0800

    Added gitignore, added support libs, fixed file structure

--- Bracelet/ViewController.m
+++ Bracelet/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "LGBluetooth.h"
 
 @interface ViewController ()
 
@@ -16,8 +17,59 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [LGCentralManager sharedInstance];
+
+    // Scaning 4 seconds for peripherals
+    [[LGCentralManager sharedInstance] scanForPeripheralsByInterval:4
+                                                         completion:^(NSArray *peripherals)
+     {
+         // If we found any peripherals sending to test
+         if (peripherals.count) {
+             [self testPeripheral:peripherals[0]];
+         }
+     }];
+
 }
+
+- (void)testPeripheral:(LGPeripheral *)peripheral
+{
+    // First of all connecting to peripheral
+    [peripheral connectWithCompletion:^(NSError *error) {
+        // Discovering services of peripheral
+        [peripheral discoverServicesWithCompletion:^(NSArray *services, NSError *error) {
+            for (LGService *service in services) {
+                // Finding out our service
+                if ([service.UUIDString isEqualToString:@"5ec0"]) {
+                    // Discovering characteristics of our service
+                    [service discoverCharacteristicsWithCompletion:^(NSArray *characteristics, NSError *error) {
+                        // We need to count down completed operations for disconnecting
+                        __block int i = 0;
+                        for (LGCharacteristic *charact in characteristics) {
+                            // cef9 is a writabble characteristic, lets test writting
+                            if ([charact.UUIDString isEqualToString:@"cef9"]) {
+                                [charact writeByte:0xFF completion:^(NSError *error) {
+                                    if (++i == 3) {
+                                        // finnally disconnecting
+                                        [peripheral disconnectWithCompletion:nil];
+                                    }
+                                }];
+                            } else {
+                                // Other characteristics are readonly, testing read
+                                [charact readValueWithBlock:^(NSData *data, NSError *error) {
+                                    if (++i == 3) {
+                                        // finally disconnect
+                                        [peripheral disconnectWithCompletion:nil];
+                                    }
+                                }];
+                            }
+                        }
+                    }];
+                }
+            }
+        }];
+    }];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
